627ae46658c40446b0027cfdf024a04637cf4be9
tshowers <ty.showers@taliferro.com>
2019-06-11 10:15:00 -0700

    integration, latest-post, newsletter, parallax, prices, review widgets

--- projects/licky-tester/src/app/licky-marketing/licky-marketing.module.ts
+++ projects/licky-tester/src/app/licky-marketing/licky-marketing.module.ts
@@ -14,29 +14,48 @@ import { LickMarketingAboutProduct5Module } from 'lick-marketing-about-product5'
 import { LickMarketingAboutProduct6Module } from 'lick-marketing-about-product6';
 import { LickMarketingAboutProduct7Module } from 'lick-marketing-about-product7';
 
-import { LickMarketingContactUs1Module} from 'lick-marketing-contact-us1';
+import { LickMarketingContactUs1Module } from 'lick-marketing-contact-us1';
 
 import { LickMarketingFaq1Module } from 'lick-marketing-faq1';
 import { LickMarketingFaq2Module } from 'lick-marketing-faq2';
 import { LickMarketingFaq3Module } from 'lick-marketing-faq3';
 
-import { LickMarketingFeatures1Module} from 'lick-marketing-features1';
-import { LickMarketingFeatures2Module} from 'lick-marketing-features2';
-import { LickMarketingFeatures3Module} from 'lick-marketing-features3';
-import { LickMarketingFeatures4Module} from 'lick-marketing-features4';
-import { LickMarketingFeatures5Module} from 'lick-marketing-features5';
+import { LickMarketingFeatures1Module } from 'lick-marketing-features1';
+import { LickMarketingFeatures2Module } from 'lick-marketing-features2';
+import { LickMarketingFeatures3Module } from 'lick-marketing-features3';
+import { LickMarketingFeatures4Module } from 'lick-marketing-features4';
+import { LickMarketingFeatures5Module } from 'lick-marketing-features5';
 
-import { LickMarketingFooter1Module} from 'lick-marketing-footer1';
+import { LickMarketingFooter1Module } from 'lick-marketing-footer1';
 
-import { LickMarketingHeader1Module} from 'lick-marketing-header1';
-import { LickMarketingHeader2Module} from 'lick-marketing-header2';
-import { LickMarketingHeader3Module} from 'lick-marketing-header3';
-import { LickMarketingHeader4Module} from 'lick-marketing-header4';
-import { LickMarketingHeader5Module} from 'lick-marketing-header5';
+import { LickMarketingHeader1Module } from 'lick-marketing-header1';
+import { LickMarketingHeader2Module } from 'lick-marketing-header2';
+import { LickMarketingHeader3Module } from 'lick-marketing-header3';
+import { LickMarketingHeader4Module } from 'lick-marketing-header4';
+import { LickMarketingHeader5Module } from 'lick-marketing-header5';
 
-import { LickMarketingHowTo1Module} from 'lick-marketing-how-to1';
+import { LickMarketingHowTo1Module } from 'lick-marketing-how-to1';
 
+import { LickMarketingIntegration1Module } from 'lick-marketing-integration1';
+import { LickMarketingIntegration2Module } from 'lick-marketing-integration2';
 
+import { LickMarketingLatestPost1Module } from 'lick-marketing-latest-post1';
+import { LickMarketingLatestPost2Module } from 'lick-marketing-latest-post2';
+
+import { LickMarketingNewsletter1Module} from 'lick-marketing-newsletter1';
+
+import { LickMarketingParallax1Module} from 'lick-marketing-parallax1';
+
+import { LickMarketingPrices1Module} from 'lick-marketing-prices1';
+import { LickMarketingPrices2Module} from 'lick-marketing-prices2';
+
+import { LickMarketingReviews1Module} from 'lick-marketing-reviews1';
+import { LickMarketingReviews2Module} from 'lick-marketing-reviews2';
+import { LickMarketingReviews3Module} from 'lick-marketing-reviews3';
+
+import {LickMarketingTeam1Module} from 'lick-marketing-team1';
+import {LickMarketingTeam2Module} from 'lick-marketing-team2';
+import {LickMarketingTeam3Module} from 'lick-marketing-team3';
 
 @NgModule({
   declarations: [HeaderWidgetsComponent, GeneralWidgetsComponent],
@@ -74,6 +93,26 @@ import { LickMarketingHowTo1Module} from 'lick-marketing-how-to1';
     LickMarketingHeader5Module,
 
     LickMarketingHowTo1Module,
+
+    LickMarketingIntegration1Module,
+    LickMarketingIntegration2Module,
+
+    LickMarketingLatestPost1Module,
+    LickMarketingLatestPost2Module,
+
+    LickMarketingNewsletter1Module,
+    LickMarketingParallax1Module,
+
+    LickMarketingPrices1Module,
+    LickMarketingPrices2Module,
+
+    LickMarketingReviews1Module,
+    LickMarketingReviews2Module,
+    LickMarketingReviews3Module,
+
+    LickMarketingTeam1Module,
+    LickMarketingTeam2Module,
+    LickMarketingTeam3Module,
   ]
 })
 export class LickyMarketingModule { }
